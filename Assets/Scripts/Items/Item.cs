804bb83623ea92347bd3d085cac9b4223ecd216f
aurelien-le-roch <51359011+aurelien-le-roch@users.noreply.github.com>
2020-03-09 21:49:30 +0100

    Add sprite field to Item.cs and ItemDefinition.cs
    this sprite will be use on the UI

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Items
 {
@@ -6,11 +8,14 @@ namespace Items
     public class Item
     {
         public string Name { get; }
+        
+        public Sprite Sprite { get; }
         public int Durability { get; }
 
-        public Item(string name, int durability)
+        public Item(string name,Sprite sprite, int durability)
         {
             Name = name;
+            Sprite = sprite;
             Durability = durability;
         }
     }
--- Assets/Scripts/Items/ItemDefinition.cs
+++ Assets/Scripts/Items/ItemDefinition.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace Items
 {
@@ -8,9 +9,10 @@ namespace Items
         [SerializeField] private string _name;
         [SerializeField] private int _durability;
         [SerializeField] private ItemType _itemType;
+        [SerializeField] private Sprite _sprite;
         public Item GetItem()
         {
-            var item = new Item(_name, _durability);
+            var item = new Item(_name,_sprite, _durability);
             return item;
         }
     }
