d05ffdd8a49cb805864a7e282d4bd2152b79a1aa
aurelien-le-roch <51359011+aurelien-le-roch@users.noreply.github.com>
2020-03-09 23:02:31 +0100

    Script That handle the UIInventory

---
+++ Assets/Scripts/UI/UIInventory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Items;
+using UnityEngine;
+
+public class UIInventory : MonoBehaviour
+{
+    [SerializeField]private UIInventorySlot[] _slots;
+
+    private Inventory _inventory;
+
+    
+    private void Start()//Inventory is create on player Awake
+    {
+        _inventory = FindObjectOfType<Player>().Inventory;
+        
+        _inventory.OnAddItem += AddItemToAvailableSlot;
+    }
+
+    private void OnDestroy()
+    {
+        _inventory.OnAddItem -= AddItemToAvailableSlot;
+    }
+    
+    private void AddItemToAvailableSlot(Item item)
+    {
+        var findAvailableSlot=false;
+
+        foreach (var slot in _slots)
+        {
+            if (!slot.IsEmpty) continue;
+            
+            slot.SetItemAndRefreshUI(item);
+            findAvailableSlot = true;
+            break;
+        }
+        
+        if(findAvailableSlot==false)
+            Debug.LogWarning("need more slot",this);
+        
+    }
+}
