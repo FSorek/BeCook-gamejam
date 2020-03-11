1d74093ba27a7b14eb781bb51c5eff9130bbb9db
fsorek <filip.sorek96@gmail.com>
2020-03-11 22:42:28 +0100

    Created a simple recipe selection feature

---
+++ Assets/Scripts/UI/UIRecipe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIRecipe : MonoBehaviour, ISelectHandler
+{
+    public static Action<Recipe> OnRecipeSelected = delegate {  };
+
+    private Recipe assignedRecipe;
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        OnRecipeSelected(assignedRecipe);
+        Debug.Log("Selected a recipe.");
+    }
+}
