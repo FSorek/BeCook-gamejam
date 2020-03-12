d5c802d349ddf1e8f4808b4136287f168327c1ec
aurelien-le-roch <51359011+aurelien-le-roch@users.noreply.github.com>
2020-03-12 14:15:56 +0100

    rename the script that openclose inventory for generic name
    will be use to open close inventory panel but also recipe book panel

---
+++ Assets/Scripts/UI/UIEnableDisablePanel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIEnableDisablePanel : MonoBehaviour
+{
+    [SerializeField] private GameObject _panel;
+
+    private bool _panelIsEnable => _panel.activeSelf;
+    
+    public void EnableDisablePanel()
+    {
+        _panel.gameObject.SetActive(!_panelIsEnable);
+    }
+}
