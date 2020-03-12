c9545c6f8887aa9385a51d3cee80d8303ba70d3f
aurelien-le-roch <51359011+aurelien-le-roch@users.noreply.github.com>
2020-03-12 18:10:01 +0100

    Create UIWorkbench script and add IOpenCloseUIPanel

---
+++ Assets/Scripts/UI/UIWorkbench.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIWorkbench : MonoBehaviour,IOpenCloseUIPanel
+{
+    [SerializeField] private GameObject _uiWorkbenchPanel;
+    public void OpenUIPanel()
+    {
+        _uiWorkbenchPanel.SetActive(true);
+    }
+
+    public void CloseUIPanel()
+    {
+        _uiWorkbenchPanel.SetActive(false);
+    }
+}
