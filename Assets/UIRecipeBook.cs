9b8fd40530fb215a849f4a315ef9009e9f0aecf5
aurelien-le-roch <51359011+aurelien-le-roch@users.noreply.github.com>
2020-03-12 15:16:03 +0100

    UIRecipeBook, add RefreshRecipesCanCraftState + deregister OnRecipeAdded
    OnResourcesChanged (event launch in ResourceBag) the UIRecipeBook will tell all the UIRecipe to update their CanCraftState(for now just display or not a cross image)
    
    note :  not the best but it work for now to show some info

--- Assets/UIRecipeBook.cs
+++ Assets/UIRecipeBook.cs
@@ -1,24 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class UIRecipeBook : MonoBehaviour
 {
     [SerializeField] private RectTransform _bookContent;
     [SerializeField] private UIRecipe _uiRecipePrefab;
+
+    private RecipeBook _recipeBook;
+    private ResourceBag _resourceBag;
+
+    private List<UIRecipe> _uiRecipes=new List<UIRecipe>();
     private void Start()
     {
         var player = FindObjectOfType<Player>();
-        player.RecipeBook.OnRecipeAdded += CreateUIRecipe;
+        _recipeBook = player.RecipeBook;
+        _resourceBag = player.ResourceBag;
+        
+        _recipeBook.OnRecipeAdded += CreateUIRecipe;
+        _resourceBag.OnResourcesChanged += RefreshRecipesCanCraftState;
 
         foreach (var recipe in player.RecipeBook.Recipes)
         {
             CreateUIRecipe(recipe);
         }
     }
+    private void OnDestroy()
+    {
+        _recipeBook.OnRecipeAdded -= CreateUIRecipe;
+        _resourceBag.OnResourcesChanged += RefreshRecipesCanCraftState;
+    }
 
     private void CreateUIRecipe(Recipe recipe)
     {
         UIRecipe uiRecipe = Instantiate(_uiRecipePrefab, _bookContent);
         uiRecipe.Initialize(recipe);
+        _uiRecipes.Add(uiRecipe);
+    }
+
+    private void RefreshRecipesCanCraftState()
+    {
+        foreach (var uiRecipe in _uiRecipes)
+        {
+            uiRecipe.RefreshCanCraftState(_resourceBag);
+        }
     }
 }
