cd46f092cc4b8df22cc11b44d0a8df76eb3cc4f5
aurelien-le-roch <51359011+aurelien-le-roch@users.noreply.github.com>
2020-03-12 20:09:01 +0100

    UIWorkvench script, add ChangeActiveRecipe() show the current selected UIRecipe in UIWorkbench

--- Assets/Scripts/UI/UIWorkbench.cs
+++ Assets/Scripts/UI/UIWorkbench.cs
@@ -1,10 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UIWorkbench : MonoBehaviour,IOpenCloseUIPanel
 {
     [SerializeField] private GameObject _uiWorkbenchPanel;
+
+    [SerializeField]private Transform _activeUiRecipeParent;
+    [SerializeField] private UIRecipe _uiRecipePrefab;
+    private UIRecipe _activeUiRecipe;
+
+    private void Awake()
+    {
+        UIRecipe.OnRecipeSelected += ChangeActiveRecipe;
+    }
+    
+    private void OnDestroy()
+    {
+        UIRecipe.OnRecipeSelected += ChangeActiveRecipe;
+    }
+
+    private void ChangeActiveRecipe(Recipe newActiveRecipe)
+    {
+        if(_activeUiRecipe!=null)
+            Destroy(_activeUiRecipe.gameObject);
+        
+        _activeUiRecipe = Instantiate(_uiRecipePrefab, _activeUiRecipeParent);
+        _activeUiRecipe.Initialize(newActiveRecipe);
+    }
+    
     public void OpenUIPanel()
     {
         _uiWorkbenchPanel.SetActive(true);
@@ -13,5 +38,11 @@ public class UIWorkbench : MonoBehaviour,IOpenCloseUIPanel
     public void CloseUIPanel()
     {
         _uiWorkbenchPanel.SetActive(false);
+
+        if (_activeUiRecipe != null)
+        {
+            Destroy(_activeUiRecipe.gameObject);
+            _activeUiRecipe = null;
+        }
     }
 }
