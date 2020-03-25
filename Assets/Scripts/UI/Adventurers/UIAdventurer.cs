d602eb12e43755a7ba8a33a6a55c348987adfe85
aurelien-le-roch <51359011+aurelien-le-roch@users.noreply.github.com>
2020-03-25 22:40:13 +0100

    UIAdventurer, Monobehaviour that hold Adventurer referenceand show adventurer sprite

---
+++ Assets/Scripts/UI/Adventurers/UIAdventurer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class UIAdventurer : MonoBehaviour
+{
+    [SerializeField] private Image _image;
+    private Adventurer _adventurer;
+    
+    public void Initialize(Adventurer adventurer)
+    {
+        _adventurer = adventurer;
+        _image.sprite = _adventurer.Sprite;
+    }
+
+
+}
