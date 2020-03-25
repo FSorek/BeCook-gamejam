8437032dd4493e1d0e7d6b13f84839b5d88cf35d
aurelien-le-roch <51359011+aurelien-le-roch@users.noreply.github.com>
2020-03-25 21:42:07 +0100

    Create Adventurers.cs => create the 4 adventurers at awake from a list of adventurersDefinition

---
+++ Assets/Scripts/Adventurers/Adventurers.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Adventurers : MonoBehaviour
+{
+    [SerializeField] private List<AdventurersDefinition> _adventurersDefinitions;
+    private List<Adventurer> _adventurers;
+
+    public List<Adventurer> AdventurersList => _adventurers;
+
+    private void Awake()
+    {
+        CreateAdventurers();
+    }
+
+    private void CreateAdventurers()
+    {
+        _adventurers=new List<Adventurer>();
+        foreach (var adventurersDefinition in _adventurersDefinitions)
+        {
+            var newAdventurer = adventurersDefinition.GetAdventurer();
+            _adventurers.Add(newAdventurer);
+        }
+    }
+}
