dc10a3860ddcd810a5cf4b7be1d79606ebfa3d26
aurelien-le-roch <51359011+aurelien-le-roch@users.noreply.github.com>
2020-03-25 22:52:30 +0100

    Create UICloseButton

---
+++ Assets/Scripts/UI/UICloseButton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class UICloseButton : MonoBehaviour
+{
+    [SerializeField] private GameObject _gameObjectToClose;
+
+    private void Awake()
+    {
+        GetComponent<Button>().onClick.AddListener((() => _gameObjectToClose.SetActive(false)));
+    }
+}
