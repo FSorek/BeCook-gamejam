73e00b38161d90573557d8c6ec93b9093d622ac5
aurelien-le-roch <51359011+aurelien-le-roch@users.noreply.github.com>
2020-03-25 21:40:45 +0100

    Create Adventurer.cs (name,sprite and adventurerInventory)

---
+++ Assets/Scripts/Adventurers/Adventurer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Adventurer
+{
+    public readonly string Name;
+    public readonly Sprite Sprite ;
+
+    public readonly AdventurerInventory AdventurerInventory;
+    public Adventurer(string name,Sprite sprite)
+    {
+        Name = name;
+        Sprite = sprite;
+        AdventurerInventory=new AdventurerInventory();
+    }
+}
