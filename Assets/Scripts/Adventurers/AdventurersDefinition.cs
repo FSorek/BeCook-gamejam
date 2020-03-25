8393d58af01aa446ead4384615a5103d44f9bd0c
aurelien-le-roch <51359011+aurelien-le-roch@users.noreply.github.com>
2020-03-25 21:41:20 +0100

    Create AdventurersDefinition.cs (definition for adventurer=>sprite and name)

---
+++ Assets/Scripts/Adventurers/AdventurersDefinition.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Adventurer Definition")]
+public class AdventurersDefinition : ScriptableObject
+{
+    [SerializeField] private string _name;
+    [SerializeField] private Sprite _sprite;
+
+    public Adventurer GetAdventurer()
+    {
+        var adventurer = new Adventurer(_name,_sprite);
+        return adventurer;
+    }
+}
