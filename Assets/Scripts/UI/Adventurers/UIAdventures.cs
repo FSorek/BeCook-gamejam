e5b2f5c1c9b5ba28f326dbc3f6357f278be676e5
aurelien-le-roch <51359011+aurelien-le-roch@users.noreply.github.com>
2020-03-25 22:42:42 +0100

    Create UIAdventures, Create UIAdventure from Adventures.Cs and handle witch UIAdventure is enable

---
+++ Assets/Scripts/UI/Adventurers/UIAdventures.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIAdventures : MonoBehaviour
+{
+    [SerializeField] private UIAdventurer _uiAdventurerPrefab;
+    [SerializeField] private UIAdventurerInventory _uiAdventurerInventoryPrefab;
+    [SerializeField] private RectTransform _uiAdventurerParent;
+    [SerializeField] private RectTransform _uiAdventurerInventoryParent;
+
+    private UIAdventurerInventory _currentActiveInventory;
+
+    private void Start()
+    {
+        var adventurers = FindObjectOfType<Adventurers>();
+        CreateUIAdventurers(adventurers);
+    }
+
+    private void CreateUIAdventurers(Adventurers adventurers)
+    {
+        var adventurersList = adventurers.AdventurersList;
+
+        foreach (var adventurer in adventurersList)
+        {
+            var uiAdventurer = Instantiate(_uiAdventurerPrefab, _uiAdventurerParent);
+            var uiAdventurerInventory = Instantiate(_uiAdventurerInventoryPrefab, _uiAdventurerInventoryParent);
+            
+            uiAdventurer.Initialize(adventurer);
+            uiAdventurerInventory.Initialize(adventurer);
+
+            uiAdventurer.gameObject.name = adventurer.Name;
+            uiAdventurerInventory.gameObject.name = adventurer.Name + " Inventory";
+            
+            uiAdventurer.GetComponent<Button>().onClick.AddListener((() => ChangeActiveInventory(uiAdventurerInventory)));
+        }
+    }
+
+    private void ChangeActiveInventory(UIAdventurerInventory newActiveInventory)
+    {
+        if (_currentActiveInventory != null)
+        {
+            _currentActiveInventory.gameObject.SetActive(false);
+        }
+        
+        newActiveInventory.gameObject.SetActive(true);
+        _currentActiveInventory = newActiveInventory;
+    }
+}
